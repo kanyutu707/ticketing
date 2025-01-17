9d607c81e168e19355755436d440ad48df38da19
Alfred Kanyutu <kanyutualfred707@gmail.com>
2025-01-17 21:03:24 +0300

    json server

--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -1,6 +1,29 @@
 import { GiPadlock } from 'react-icons/gi'
 import './Hero.css'
+import { useState } from 'react'
 const Hero = () => {
+  const [jsonData, setJsonData]=useState([]);
+  const [newEmail, setNewEmail]=useState("");
+  function handleSubmit(e:any){
+    e.preventDefault();
+    const email=newEmail.trim();
+    if (email){
+      fetch("http://localhost:5000/waitlist", {
+        method:"POST",
+        body:JSON.stringify({
+          email,
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      })
+      .then(response=>response.json())
+      .then(data=>{
+        console.log(data);
+      })
+    }
+   
+  }
   return (
     <div className="herocontainer">
         <nav>
@@ -14,7 +37,10 @@ const Hero = () => {
         <span>
             TIMET, A TIMELESS TICKET
         </span>
-        <button>GET STARTED</button>
+        <form className='waitlist' onSubmit={handleSubmit}>
+          <input type="email"  value={newEmail} onChange={e=>setNewEmail(e.target.value)}/>
+          <button>JOIN THE WAIT LIST</button>
+          </form>
         </div>
     </div>
   )
