839e7bdf93a8d7aa7fbb3696cbf60f92f86a31de
Alfred Kanyutu <kanyutualfred707@gmail.com>
2025-01-17 22:21:56 +0300

    updates

--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -1,56 +1,55 @@
-import { GiPadlock } from 'react-icons/gi'
-import './Hero.css'
-import { useState } from 'react'
+import { GiPadlock } from 'react-icons/gi';
+import './Hero.css';
+import { useState } from 'react';
+
 const Hero = () => {
+    const [newEmail, setNewEmail] = useState("");
+    const apiKey = import.meta.env.VITE_API_KEY;
+    const apiUrl = import.meta.env.VITE_API_URL;
+    console.log("API Key:", apiKey);
+    console.log("API URL:", apiUrl);
 
-  const [newEmail, setNewEmail]=useState("");
-  const apiKey = import.meta.env.VITE_API_KEY;;
-const apiUrl = import.meta.env.VITE_API_URL;
-
-console.log("API Key:", apiKey);
-console.log("API URL:", apiUrl);
-  function handleSubmit(e:any){
-    e.preventDefault();
-    const email=newEmail.trim();
-    if (email){
-      fetch(`${apiUrl}/waitlist`, {
-        method:"POST",
-       headers:{
-        'Content-Type': 'application/json',
-        'X-Master-key': `${apiKey}`,
-       },
-        body:JSON.stringify({
-          email,
-        }),
-       
-      })
-      .then(response=>response.json())
-      .then(data=>{
-        console.log(data);
-      })
+    function handleSubmit(e:any) {
+        e.preventDefault();
+        const email = newEmail.trim();
+        if (email) {
+            fetch(`${apiUrl}/waitlist`, {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-Master-key': `${apiKey}`,
+                },
+                body: JSON.stringify({
+                    email,
+                }),
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+            });
+        }
     }
-   
-  }
-  return (
-    <div className="herocontainer">
-        <nav>
-            <span>TICKETING SYSTEM</span>
-            <GiPadlock title='GET STARTED'/>
-        </nav>
-        <div className='content'> 
-        <section >
-        TIMET is an online e-ticketing system that helps businesses manage their tickets more efficiently. With TIMET, you can create tickets, view existing ones, and track their status.
-        </section>
-        <span>
-            TIMET, A TIMELESS TICKET
-        </span>
-        <form className='waitlist' onSubmit={handleSubmit}>
-          <input type="email"  value={newEmail} onChange={e=>setNewEmail(e.target.value)}/>
-          <button>JOIN THE WAIT LIST</button>
-          </form>
+
+    return (
+        <div className="herocontainer">
+            <nav>
+                <span>TICKETING SYSTEM</span>
+                <GiPadlock title='GET STARTED' />
+            </nav>
+            <div className='content'>
+                <section>
+                    TIMET is an online e-ticketing system that helps businesses manage their tickets more efficiently. With TIMET, you can create tickets, view existing ones, and track their status.
+                </section>
+                <span>
+                    TIMET, A TIMELESS TICKET
+                </span>
+                <form className='waitlist' onSubmit={handleSubmit}>
+                    <input type="email" value={newEmail} onChange={e => setNewEmail(e.target.value)} />
+                    <button>JOIN THE WAIT LIST</button>
+                </form>
+            </div>
         </div>
-    </div>
-  )
+    );
 }
 
-export default Hero
+export default Hero;
